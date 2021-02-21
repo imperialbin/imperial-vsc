f7df78385ab424270ea835d86f469036e2a422fc
devlooskie <hello@looskie.com>
2021-02-21 18:13:42 -0500

    forgot to put const in front of selection and rename selection to btnSelection

--- extension.js
+++ extension.js
@@ -5,51 +5,61 @@ const Imperial = require("imperial-node").Imperial;
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    const disposable = vscode.commands.registerCommand("imperial.uploadCode", () => {
-        const config = vscode.workspace.getConfiguration("imperial");
-        const editor = vscode.window.activeTextEditor;
-        // where da editor go?
-        if(!editor) return;
-        // Current highlighted code.
-        const selectedCode = editor.document.getText(editor.selection);
+  const disposable = vscode.commands.registerCommand(
+    "imperial.uploadCode",
+    () => {
+      const config = vscode.workspace.getConfiguration("imperial");
+      const editor = vscode.window.activeTextEditor;
+      // where da editor go?
+      if (!editor) return;
+      // Current highlighted code.
+      const selectedCode = editor.document.getText(editor.selection);
 
-        const imperialAPI = new Imperial(config.apiToken);
-        const documentSettings = {
-            longerUrls: config.longerUrls || false,
-            instantDelete: config.instantDelete || false,
-            imageEmbed: config.imageEmbed || false,
-            expiration: config.expiration || 5,
-        };
+      const imperialAPI = new Imperial(config.apiToken);
+      const documentSettings = {
+        longerUrls: config.longerUrls || false,
+        instantDelete: config.instantDelete || false,
+        imageEmbed: config.imageEmbed || false,
+        expiration: config.expiration || 5,
+      };
 
-        imperialAPI.createDocument(selectedCode, documentSettings, async (err, document) => {
-            if(err) return vscode.window.showInformationMessage("This upload failed, you may be getting rate limited!");
-            const selection = await vscode.window.showInformationMessage(
-                /* Text */
-                `Uploaded selected text to Imperial!\n${document.formattedLink}`,
-                /* Buttons */
-                "Copy Link",
-                "Open Document"
+      imperialAPI.createDocument(
+        selectedCode,
+        documentSettings,
+        async (err, document) => {
+          if (err)
+            return vscode.window.showInformationMessage(
+              "This upload failed, you may be getting rate limited!"
             );
-            selection = selection.toLowerCase();
+          const selection = await vscode.window.showInformationMessage(
+            /* Text */
+            `Uploaded selected text to Imperial!\n${document.formattedLink}`,
+            /* Buttons */
+            "Copy Link",
+            "Open Document"
+          );
 
-            if (selection == "copy link") {
-                return await vscode.env.clipboard.writeText(document.formattedLink);
-            } else if (selection == "open document") {
-                return await vscode.commands.executeCommand(
-                    "vscode.open",
-                    vscode.Uri.parse(document.formattedLink)
-                );
-            }
-        });
-    });
+          const btnSelection = selection.toLowerCase();
+          if (btnSelection == "copy link") {
+            return await vscode.env.clipboard.writeText(document.formattedLink);
+          } else if (btnSelection == "open document") {
+            return await vscode.commands.executeCommand(
+              "vscode.open",
+              vscode.Uri.parse(document.formattedLink)
+            );
+          }
+        }
+      );
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-    activate,
-    deactivate,
+  activate,
+  deactivate,
 };
