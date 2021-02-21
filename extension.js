3b3b0d9ffc6f7574c1b95ea916575d209b66fc4b
Tech <debitcardzpaypal@gmail.com>
2021-02-21 17:24:51 -0500

    refactor: improved upon current code

--- extension.js
+++ extension.js
@@ -4,69 +4,52 @@ const Imperial = require("imperial-node").Imperial;
 /**
  * @param {vscode.ExtensionContext} context
  */
-
 function activate(context) {
-  let disposable = vscode.commands.registerCommand(
-    "imperial.uploadCode",
-    function () {
-      const config = vscode.workspace.getConfiguration("imperial");
-      const editor = vscode.window.activeTextEditor;
+    const disposable = vscode.commands.registerCommand("imperial.uploadCode", () => {
+        const config = vscode.workspace.getConfiguration("imperial");
+        const editor = vscode.window.activeTextEditor;
+        // where da editor go?
+        if(!editor) return;
+        // Current highlighted code.
+        const selectedCode = editor.document.getText(editor.selection);
 
-      // Check if the editor is active
-      if (!editor) return;
+        const imperialAPI = new Imperial(config.apiToken);
+        const documentSettings = {
+            longerUrls: config.longerUrls || false,
+            instantDelete: config.instantDelete || false,
+            imageEmbed: config.imageEmbed || false,
+            expiration: config.expiration || 5,
+        };
 
-      // Get the text/code selected.
-      const code = editor.document.getText(editor.selection);
+        imperialAPI.createDocument(selectedCode, documentSettings, async (err, document) => {
+            if(err) return vscode.window.showInformationMessage("This upload failed, you may be getting rate limited!");
+            const selection = await vscode.window.showInformationMessage(
+                /* Text */
+                `Uploaded selected text to Imperial!\n${document.formattedLink}`,
+                /* Buttons */
+                "Copy Link",
+                "Open Document"
+            );
+            selection = selection.toLowerCase();
 
-      // Set API token
-      const api = new Imperial(config.apiToken);
-
-      // Document settings
-      const documentSettings = {
-        longerUrls: config.longerUrls || false,
-        instantDelete: config.instantDelete || false,
-        imageEmbed: config.imageEmbed || false,
-        expiration: config.expiration || 5,
-      };
-
-      // Create the document with the document settings
-      api.createDocument(code, documentSettings, (err, document) => {
-        // If rate limited/internal server error
-        if (err)
-          return vscode.window.showInformationMessage(
-            "Upload failed! You're probably being rate limited!"
-          );
-
-        // If succeeds
-        vscode.window
-          .showInformationMessage(
-            `Upload succeeded! \n ${document.formattedLink}`,
-            "Copy link",
-            "Open document"
-          )
-          .then(async (selection) => {
-            // If user selects copy link
-            if (selection === "Copy link") {
-              await vscode.env.clipboard.writeText(document.formattedLink);
-            } else if (selection === "Open document") {
-              // If user selects open document
-              await vscode.commands.executeCommand(
-                "vscode.open",
-                vscode.Uri.parse(document.formattedLink)
-              );
+            if (selection == "copy link") {
+                return await vscode.env.clipboard.writeText(document.formattedLink);
+            } else if (selection == "open document") {
+                return await vscode.commands.executeCommand(
+                    "vscode.open",
+                    vscode.Uri.parse(document.formattedLink)
+                );
             }
-          });
-      });
-    }
-  );
+        });
+    });
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-  activate,
-  deactivate,
+    activate,
+    deactivate,
 };
